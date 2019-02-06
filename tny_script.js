7d8e65828752fc693a93a81c25833e1e574fc545
Unknown <mjohnson030@west-mec.org>
2019-02-05 18:14:50 -0700

    Origin of Javascript
    Today, we applied a few basic principles of javascript. Embedded script element into HTML so that our javascript written in the .js will run properly. Started learning how to use debugging tricks like developer tools in order to fix our code. Added single-line and multi-line comments.

--- tny_script.js
+++ tny_script.js
@@ -1,11 +1,15 @@
+"use strict";
+
 /*
    New Perspectives on HTML5 and CSS3, 7th Edition
    Tutorial 9
    Tutorial Case
 
    Countdown Clock
-   Author: 
-   Date:   
+   Author: Millione Johnson
+   Date: 2/5/19  
 
 */
+
+ window.alert("Hi welcome to chili's");
 
